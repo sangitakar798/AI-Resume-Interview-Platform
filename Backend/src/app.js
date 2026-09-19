2d88330bd8ad1c9d80c1b39f556db67487eeb540
Sangita kar <sangitakar798@gmail.com>
2026-09-20 03:34:01 +0530

    Allow Vercel frontend URL in CORS config

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,9 +5,12 @@ const cors = require('cors')
 app.use(cookieParser())
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+  origin: [
+    'https://ai-resume-interview-platform-io6a.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 
 
 
